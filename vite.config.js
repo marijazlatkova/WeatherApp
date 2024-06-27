a23943a91c24f402db26a7d33a88897ef6200af2
Marija Zlatkova <marijazlatkova44@yahoo.com>
2024-06-27 21:27:37 +0200

    add .env configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": JSON.stringify(process.env),
+  },
   server: {
     port: 3000,
   },
